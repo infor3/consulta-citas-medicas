48493785e6707075cd67d208ead251b84e1a8e2f
Rolando <rol0priest@gmail.com>
2015-11-21 08:48:06 -0500

    actualizacion

--- src/main/java/Negocio/AdmiAgenda.java
+++ src/main/java/Negocio/AdmiAgenda.java
@@ -6,14 +6,13 @@ import Datos.Medico;
 
 import java.util.ArrayList;
 
-/**
- * Created by Andrew on 12/11/2015.
- */
+
 public class AdmiAgenda {
 
     public void listadoCitas(Hospital hospital){
         ArrayList<Cita> totalCitas=new ArrayList<>();
-        ArrayList<Medico> listaMedicos=new ArrayList<>();
+        ArrayList<Medico> listaMedicos;
+        listaMedicos = new ArrayList<>();
         for (Medico listaMedico : listaMedicos) {
             for (int j = 0; j < listaMedico.getAgenda().size(); j++) {
                 for (int k = 0; k < listaMedico.getAgenda().get(j).getListaCitas().size(); k++) {
