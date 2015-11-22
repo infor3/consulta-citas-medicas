059a9ce5ffe835e15ae265cbaaa1c71864a717ef
Inkler <inkler19@gmail.com>
2015-11-21 21:40:50 -0500

    Eliminación atributo afiliado de Paciente

--- src/main/java/Datos/Paciente.java
+++ src/main/java/Datos/Paciente.java
@@ -3,22 +3,12 @@ package Datos;
 import java.util.ArrayList;
 
 public class Paciente extends Persona{
-	private boolean afiliado;
 	private EPS eps;
 	private ArrayList <Cita> listaCitas=new ArrayList<>();
 
 	public Paciente(String nombre, String apellido, String correo, String direccion, String id, String contrasenia, int nivel, int edad, int telefono, int celular, boolean afiliado, EPS eps) {
 		super(nombre, apellido, correo, direccion, id, contrasenia, nivel, edad, telefono, celular);
-		this.afiliado = afiliado;
 		this.eps = eps;
-	}
-
-	public boolean isAfiliado() {
-		return afiliado;
-	}
-
-	public void setAfiliado(boolean afiliado) {
-		this.afiliado = afiliado;
 	}
 
 	public ArrayList<Cita> getListaCitas() {
