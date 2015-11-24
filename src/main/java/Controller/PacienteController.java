a0ddda78823bd734551d0cd64ecf25c3de1c565c
andresfelipe <afdiazg@unboque.edu.com>
2015-11-24 00:28:47 -0500

    pacienteController y formulario paciente

--- src/main/java/Controller/PacienteController.java
+++ src/main/java/Controller/PacienteController.java
@@ -11,7 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "formulariopaciente", urlPatterns = "/formularioPaciente.html")
+@WebServlet("/PacienteController")
+//@WebServlet(name = "formulariopaciente", urlPatterns = "/formularioPaciente.html")
 public class PacienteController extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
@@ -24,5 +25,37 @@ public class PacienteController extends HttpServlet {
         } else {
             req.setAttribute("valido", false);
         }
+
+
+
+
+        String nom=req.getParameter("nombre");
+
+        String apellido=req.getParameter("apellidos");
+        String fechaN=req.getParameter("fecha_nacimiento");
+        String correo=req.getParameter("correo");
+        String direc=req.getParameter("direccion");
+
+        // ESTE ES EL ERROR LOS ENTEROS NO SE POR QUE JAJAJA
+        //int telefono=Integer.parseInt("telefono");
+        //int celular=Integer.parseInt("celular");
+        //int documen=Integer.parseInt("documento");
+
+
+
+        System.out.println(nom);
+
+        System.out.println(apellido);
+        System.out.println(fechaN);
+        System.out.println(correo);
+        System.out.println(direc);
+        //System.out.println(telefono);
+        //System.out.println(celular);
+        //System.out.println(documen);
+
+        // LA IDEA AQUI ES MANDAR TODOS LOS PARAMETROS A PACIENTE
+
+        //Paciente p= new Paciente(nombre,apellido,correo,direc,telefono,celular,documen,true,b);
+
     }
 }
